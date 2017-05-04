8eb9d19b25c6740386e3db145e46576acbcd7b1c
Will保哥 <doggy.huang@gmail.com>
2017-05-04 22:39:01 +0800

    Initial commit for day 1

---
+++ main.ts
@@ -0,0 +1,31 @@
+angular.module('app', [])
+    .controller('MainController', function() {
+        //  ng-init="PName='BMW';Price=1990000;Qty=5"
+        var vm = this;
+        
+        vm.PName = 'BMW';
+        vm.Price = 19900000;
+        vm.Qty = 5;
+
+        vm.Subtotal = function(Price, Qty) {
+            let sum = Price * Qty;
+            if(Qty >= 10) {
+                sum = sum * 0.9;
+            }
+            return sum;
+        }
+
+        vm.carts = [];
+
+        vm.add = function() {
+            vm.carts.push({
+                PName: vm.PName,
+                Price: vm.Price,
+                Qty: vm.Qty
+            })
+        }
+
+        vm.del = function(idx) {
+            vm.carts.splice(idx, 1);
+        }
+    })
